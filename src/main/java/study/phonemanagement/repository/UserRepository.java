a5d549e672e7bdad16c581e81b5822cbb2ce2044
iju42829 <ijud4282@gmail.com>
2025-05-14 23:44:38 +0900

    fix: 관리자 계정 존재 여부 확인에 비관적 락 적용

--- src/main/java/study/phonemanagement/repository/UserRepository.java
+++ src/main/java/study/phonemanagement/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package study.phonemanagement.repository;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 import study.phonemanagement.entity.user.Role;
 import study.phonemanagement.entity.user.User;
@@ -13,5 +15,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
+
+    @Lock(LockModeType.WRITE)
     boolean existsByRole(Role role);
 }
