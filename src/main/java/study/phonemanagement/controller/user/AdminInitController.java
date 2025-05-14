f3222a6d656c10aa707f174d944fb6beb0ce19d8
JaeYun <116072376+iju42829@users.noreply.github.com>
2025-05-15 00:00:06 +0900

    최초 관리자 계정 생성 기능 구현 (#5)
    * feat: 최초 관리자 계정 생성 기능 추가
    
    * fix: 관리자 계정 존재 여부 확인에 비관적 락 적용
    
    * feat: AdminInitControllerAdvice에서 AlreadyExistsAdminException 처리 로직 추가
    
    * fix: 관리자 회원가입 실패 시 잘못된 뷰 반환 문제 수정

---
+++ src/main/java/study/phonemanagement/controller/user/AdminInitController.java
@@ -0,0 +1,41 @@
+package study.phonemanagement.controller.user;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import study.phonemanagement.controller.user.request.CreateUserRequest;
+import study.phonemanagement.service.user.AdminUserService;
+
+@Slf4j
+@Controller
+@RequestMapping("/admin")
+@RequiredArgsConstructor
+public class AdminInitController {
+
+    private final AdminUserService adminUserService;
+
+    @GetMapping("/signup")
+    public String joinPage(Model model) {
+        model.addAttribute("createUserRequest", new CreateUserRequest());
+        return "users/adminJoin";
+    }
+
+    @PostMapping("/signup")
+    public String join(@Validated @ModelAttribute CreateUserRequest createUserRequest, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            log.info("bindingResult: {}", bindingResult);
+            return "users/adminJoin";
+        }
+
+        adminUserService.createAdminUser(createUserRequest);
+
+        return "redirect:/admin/login";
+    }
+}
--- src/main/java/study/phonemanagement/repository/UserRepository.java
+++ src/main/java/study/phonemanagement/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package study.phonemanagement.repository;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
+import study.phonemanagement.entity.user.Role;
 import study.phonemanagement.entity.user.User;
 
 import java.util.Optional;
@@ -12,4 +15,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
+
+    @Lock(LockModeType.WRITE)
+    boolean existsByRole(Role role);
 }
