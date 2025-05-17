b5d416cfc3087d3cea3e478e93676a009ede3500
iju42829 <ijud4282@gmail.com>
2025-05-17 19:35:45 +0900

    feat: PhoneAdvice 예외 처리 클래스 추가

--- src/main/java/study/phonemanagement/advice/phone/PhoneAdvice.java
+++ src/main/java/study/phonemanagement/advice/phone/PhoneAdvice.java
@@ -4,16 +4,27 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
-import study.phonemanagement.controller.phone.AdminPhoneController;
 import study.phonemanagement.exception.phone.PhoneNotFoundException;
+import study.phonemanagement.exception.phone.PhoneStockShortageException;
 
 @Slf4j
-@ControllerAdvice(assignableTypes = {AdminPhoneController.class})
-public class AdminPhoneControllerAdvice {
-
+@ControllerAdvice
+public class PhoneAdvice {
     @ExceptionHandler(PhoneNotFoundException.class)
     public ModelAndView handlePhoneNotFoundException(PhoneNotFoundException e) {
-        log.warn("[AdminPhoneController - exceptionHandle] ex", e);
+        log.warn("[Phone - exceptionHandle] ex", e);
+
+        ModelAndView mav = new ModelAndView("error");
+        mav.setStatus(e.getErrorCode().getHttpStatus());
+        mav.addObject("message", e.getErrorCode().getMessage());
+        mav.addObject("redirectUrl", e.getErrorCode().getRedirectUrl());
+
+        return mav;
+    }
+
+    @ExceptionHandler(PhoneStockShortageException.class)
+    public ModelAndView handlePhoneStockShortageException(PhoneStockShortageException e) {
+        log.warn("[Phone - exceptionHandle] ex", e);
 
         ModelAndView mav = new ModelAndView("error");
         mav.setStatus(e.getErrorCode().getHttpStatus());
