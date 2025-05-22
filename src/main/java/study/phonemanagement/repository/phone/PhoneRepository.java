9f7e949d244ec4bb559e2e656eba732efc41228c
iju42829 <ijud4282@gmail.com>
2025-05-22 23:21:35 +0900

    fix: 판매 가능 상태 상품에만 주문 허용

--- src/main/java/study/phonemanagement/repository/phone/PhoneRepository.java
+++ src/main/java/study/phonemanagement/repository/phone/PhoneRepository.java
@@ -3,6 +3,7 @@ package study.phonemanagement.repository.phone;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import study.phonemanagement.entity.phone.Phone;
+import study.phonemanagement.entity.phone.Status;
 
 import java.util.Optional;
 
@@ -10,4 +11,5 @@ import java.util.Optional;
 public interface PhoneRepository extends JpaRepository<Phone, Long>, PhoneRepositoryCustom {
 
     Optional<Phone> findByIdAndDeletedAtIsNull(Long id);
+    Optional<Phone> findByIdAndStatusAndDeletedAtIsNull(Long id, Status status);
 }
